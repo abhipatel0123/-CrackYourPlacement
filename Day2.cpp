40bb93b52a7944cfc0a773683c0b53bb6e97d195
abhipatel0211 <abhiptl0123@gmail.com>
2024-07-23 10:01:40 +0530

    23/7/24 2nd que done

---
+++ Day2.cpp
@@ -0,0 +1,35 @@
+//Chocolate Distribution Problem
+
+ long long findMinDiff(vector<long long> a, long long n, long long m){
+    // code
+        if(a.size() < m){
+            return 0;
+        }
+           sort(a.begin(),a.end());
+           int min=INT_MAX;
+           for(int i=0;i<=a.size()-m;i++){
+               int diff =a[i+m-1]-a[i];
+               if(diff<min ){
+                   min=diff;
+               }
+           }
+           return min;
+    
+    }
+
+
+//Two Sum 
+
+vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+ nums[j] == target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
