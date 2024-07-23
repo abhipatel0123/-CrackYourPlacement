da8d667d2e766d113247f2b0e9d5411dd0dad256
abhipatel0211 <abhiptl0123@gmail.com>
2024-07-23 13:48:37 +0530

    23/7/24 3rd que done

--- Day2.cpp
+++ Day2.cpp
@@ -33,3 +33,15 @@ vector<int> twoSum(vector<int>& nums, int target) {
         }
         return ans;
     }
+
+// Best Time to Buy and Sell Stock II
+    int maxProfit(vector<int>& prices) {
+        int sum=0;
+        int n =prices.size();
+        for(int i=0;i<n-1;i++){
+            if(prices[i+1] > prices[i]){
+                sum+=prices[i+1] - prices[i];
+            }
+        }
+        return sum;
+    }
